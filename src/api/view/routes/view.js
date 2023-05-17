8195cb34b6abc8a2e6b9041aa0eef2e2b7941e45
NatiGK <natnaelgashu2022@gmail.com>
2023-05-17 13:12:18 +0300

    generate view api

---
+++ src/api/view/routes/view.js
@@ -0,0 +1,13 @@
+module.exports = {
+  routes: [
+    // {
+    //  method: 'GET',
+    //  path: '/view',
+    //  handler: 'view.exampleAction',
+    //  config: {
+    //    policies: [],
+    //    middlewares: [],
+    //  },
+    // },
+  ],
+};
---
+++ src/api/view/services/view.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/**
+ * view service
+ */
+
+module.exports = () => ({});
